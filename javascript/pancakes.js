8a22fd607ca1da14c790d9378b560ced67677c5c
aritheroyal <atr68750@gmail.com>
2025-04-08 03:05:51 +0100

    Create pancakes.js

---
+++ javascript/pancakes.js
@@ -0,0 +1,8 @@
+function pancakes() {
+  document.getElementsByClassName("pancakesbutton");
+  document.getElementById("awesometext");
+setTimeout(function() {
+  awesometext.innerHTML = "welcome to the royalest place on the internet!";
+}, 5000);
+ awesometext.innerHTML = "Oh, am I mistaken? I thought I heard something about delicious pancakes."; 
+}
